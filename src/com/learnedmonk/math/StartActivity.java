540ba26cdc08ca3783ffb8b795de88fbea509166
Suresh Kondamudi <suresh@Sureshs-MacBook-Pro.local>
2014-04-14 18:00:12 +0530

    setup

---
+++ src/com/learnedmonk/math/StartActivity.java
@@ -0,0 +1,23 @@
+package com.learnedmonk.math;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class StartActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start);
+		
+	}
+	
+	public void onClick(View v){
+		Intent intent = new Intent(this, MainActivity.class);
+	    startActivity(intent);
+	}
+
+}
